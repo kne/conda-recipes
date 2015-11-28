9abcf59f250b5acc2eae7f1cbd60391e4851a5ee
kne <sirkne@gmail.com>
2015-11-28 12:46:31 -0500

    Add pygame_sdl2 recipe on my branch for now

---
+++ pygame_sdl2/dev/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update the tags for the version number
+git fetch origin --tags 
+
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    if [[ -z "$(which brew)" ]]; then
+        echo "Sorry, OSX requires a homebrew installation!"
+        exit 1
+    fi
+    brew install jpeg libpng freetype
+    brew install sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_net sdl2_ttf 
+    brew install portmidi smpeg
+elif [[ "$(uname)" == "Linux" ]]; then
+    if [[ -z "$(which apt-get)" ]]; then
+        echo "** warning: Can only check dependencies on ubuntu/debian"
+    else
+        echo "Checking dependencies..."
+        declare -a packages=("build-essential" "python-dev" "libsdl2-dev"
+                             "libsdl2-image-dev" "libsdl2-mixer-dev"
+                             "libsdl2-ttf-dev" "libjpeg-dev" "libpng12-dev")
+
+        for dep in "${packages[@]}"
+        do
+            inst_ver=$(apt-cache policy $dep |grep Installed: | sed 's/\s*Installed: \(.*\)$/\1/')
+            echo "Dependency $dep (installed version: $inst_ver)"
+            if [[ "$inst_ver" == "(none)" ]]; then
+                echo "Please install dependency $dep!"
+                exit 1;
+            fi
+        done
+    fi
+fi
+
+$PYTHON setup.py install
