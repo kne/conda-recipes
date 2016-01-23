ff54c19b313f3f7a609ce88379b44cd0390d68a1
kne <sirkne@gmail.com>
2016-01-23 16:29:46 -0500

    Tweak windows build scripts
    Nearly lost and forgotten from last year

--- pybox2d/dev/bld.bat
+++ pybox2d/dev/bld.bat
@@ -1,8 +1,9 @@
 if not exist C:\dev\swig goto noswig
 
-echo on
+echo off
 
-set PATH=C:\dev\swig;C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC;%PATH%
+echo Adding swig to the path
+set PATH=C:\dev\swig;%PATH%
 
 rem NOTE: git version tag broken on conda-build-1.18.2 (revert to 1.18.1 or use
 rem       the version after when it's released)
@@ -14,11 +15,11 @@ rem 	c1xx : fatal error C1083: Cannot open compiler intermediate file:
 rem           'C:\Windows\system32\bsh\tmp\_CL_96a80b23ex': No such file or directory
 
 echo.%TMP% | findstr /I /C:"system32\bsh">nul && (
-    echo "Detected MobaSSH session. Changing TMP path to C:\temp."
+    echo Detected MobaSSH session. Changing TMP path to C:\temp.
     mkdir C:\temp
     set TMP=C:\temp
 ) || (
-    echo "Running under regular command line"    
+    echo Running under regular command line
 )
 @echo on
 
@@ -29,7 +30,7 @@ if errorlevel 1 exit 1
 exit 0
 
 :noswig
-echo "Expected SWIG to be in the path C:\dev\swig"
-echo "Please install swig >= 3.0.7 there from:"
-echo "http://sourceforge.net/projects/swig/files/swigwin/swigwin-3.0.7/swigwin-3.0.7.zip/download"
+echo Expected SWIG to be in the path C:\dev\swig
+echo Please install swig >= 3.0.7 there from:
+echo http://sourceforge.net/projects/swig/files/swigwin/swigwin-3.0.7/swigwin-3.0.7.zip/download
 exit 1
